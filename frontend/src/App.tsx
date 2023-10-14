786e75edbd8ef43fcf1dd74a22bf8ea42dd1fdd4
George007361 <gera.illarionov@mail.ru>
2023-10-14 18:52:52 +0300

    one component app

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,23 +1,122 @@
 import React, {useState} from 'react';
-import NewMessageComp from "./components/newMessageComp/newMessageComp";
-import List from "./components/List/List";
-import Chat from "./components/Chat/Chat";
+import axios from "axios";
+
+interface Message {
+    attaches: string[];
+    isAuthorTeacher: boolean;
+    text: string;
+    time: string;
+}
+
+interface Chat {
+    messages: Message[];
+}
 
 function App() {
-    const [currChat, setCurrChat] = useState<Chat | null>(null);
-    const handleChatClick = (chat: Chat) => {
-        setCurrChat(chat);
+
+    const [val, setVal] = useState('');
+    const [chats, setChats] = useState<Chat[]>([]);
+    const refresh = () => {
+        axios.get('http://localhost:8080/api/chats/1')
+            .then((resp: any) => {
+                    setChats(
+                        resp.chats.forEach((chat: any): Chat => {
+                            return {
+                                messages: chat.messages.forEach((msg: any): Message => {
+                                    return {
+                                        attaches: [],
+                                        isAuthorTeacher: Boolean(msg.isAuthorTeacher),
+                                        text: msg.text,
+                                        time: String(msg.time),
+                                    }
+                                })
+                            }
+                        })
+                    );
+                }
+            )
+            .catch(err => console.log(err));
     }
+
+    const changeChat = () => {
+    }
+
+    const sendMsg = () => {
+        const msg = {
+            attaches: [],
+            text: val,
+        };
+        axios.post('http://localhost:8080/api/send', JSON.stringify(msg))
+            .then((resp: any) => {
+                    const msgForm: Message = {
+                        isAuthorTeacher: true,
+                        text: msg.text,
+                        attaches: [],
+                        time: ""
+                    }
+
+                    const arr = [...chats];
+                    arr[0].messages.push(msgForm);
+                    setChats(arr);
+                }
+            )
+            .catch(err => console.log(err));
+    }
+
     return (
-        <span
-            style={{
-                display: "flex",
-                gap: "40px",
-            }}
-        >
-       <List onChatClick={handleChatClick}/>
-       <Chat chat={currChat}/>
-   </span>
+        <>
+            {/*sidebar*/}
+            <div
+                style={{
+                    display: "flex",
+                    gap: "40px",
+                }}
+            >
+                <>
+                    <button
+                        onClick={refresh}>
+                        Refresh
+                    </button>
+
+
+                    {chats &&
+                        chats.map(chat => (
+                            <div
+                                onClick={changeChat}
+                            >
+                                Name
+                            </div>
+                        ))
+                    }
+                </>
+            </div>
+            {/*sidebar*/}
+            {/*messages*/}
+            <div>
+                {chats[0] &&
+                    chats[0].messages.map(msg => (
+                        <div>
+                            {msg.text}
+                        </div>
+                    ))
+                }
+            </div>
+            {/*messages*/}
+
+            {/*form*/}
+            <div>
+                <input
+                    onChange={(e) => setVal(e.target.value)}
+                />
+
+                <button
+                    onClick={sendMsg}
+                >
+                    Send
+                </button>
+            </div>
+            {/*form*/}
+        </>
     );
 }
 
